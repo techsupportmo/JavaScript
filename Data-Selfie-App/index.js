0af6f2c40837e0d239143d03fb9c2a5897832feb
techsupportmo <46112529+techsupportmo@users.noreply.github.com>
2022-01-07 23:14:36 -0600

    current progress
    doing some stuff with post. not done yet. must finish video

--- Data-Selfie-App/index.js
+++ Data-Selfie-App/index.js
@@ -1,11 +1,26 @@
+// ------------------------------------------
+// SERVER
+// index.js file
+// ------------------------------------------
+
 // Essentially stores the express library in variable called 'Express'
 const express = require('express');
 
 const app = express();
 
 // Using Port 3000 and prints to the console
-app.listen(3000, ()=> console.log('Listening at 3000'));
+app.listen(3000, () => console.log('Listening at 3000'));
 
 // This will allow us to display a static webpage using Express
 // Directory name is 'public'
 app.use(express.static('public'));
+
+// Using Post()
+// Function has two arguments - request & response
+// Request variable - holds everything contained within the request
+// Response variable - Lets us send things to the client
+app.post("/api", (request, response) => {
+
+    console.log(request.body);
+
+});
